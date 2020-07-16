5b93fa6158709f67bfb4637b4ccf613d2bd3ec2f
scottweil <63029463+scottweil@users.noreply.github.com>
2020-07-16 14:45:02 +0900

    JS_home_contact_scrollIntoView_function

--- navbar.js
+++ navbar.js
@@ -3,8 +3,8 @@
 const navbar = document.querySelector("#navbar");
 const navbar_colored = "navbar_colored";
 const navbar_height = navbar.getBoundingClientRect().height;
-
 const navbar__menu = navbar.querySelector("#navbar__menu");
+const home__contact = document.querySelector(".home__contact");
 
 function init() {
   // make navbar transparent and colored when scrolled
@@ -17,16 +17,20 @@ function init() {
   });
 
   // handle scrolling when tapping on the navbar__menu_item
-  navbar__menu.addEventListener("click", (event) => {
-    const target = event.target;
-    const link = event.target.dataset.link;
-    if (link == null) {
-      return;
-    }
+  navbar__menu.addEventListener("click", scrollIntoView);
 
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({ behavior: "smooth" });
-  });
+  // scroll to the contact section when click the home__contact button
+  home__contact.addEventListener("click", scrollIntoView);
 }
 
+function scrollIntoView() {
+  const link = event.target.dataset.link;
+
+  if (link == null) {
+    return;
+  }
+
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
 init();
